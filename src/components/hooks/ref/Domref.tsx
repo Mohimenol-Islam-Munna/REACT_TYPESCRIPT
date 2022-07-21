2392c67d7c942b9a2f9f0812f539030e38193539
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-07-21 12:16:57 +0600

    useRef complete

--- src/components/hooks/ref/Domref.tsx
+++ src/components/hooks/ref/Domref.tsx
@@ -2,18 +2,23 @@ import React, { useEffect, useRef } from "react";
 
 function Domref() {
   const domRef = useRef<HTMLInputElement>(null);
+  //   const domRef = useRef<HTMLInputElement>(null!);
 
   useEffect(() => {
+    console.log("domRef.current ::", domRef.current?.value);
+    // console.log("domRef.current ::", domRef.current.value);
     domRef.current?.focus();
+    // domRef.current.focus();
   }, []);
 
   return (
     <div className="w-[50%] mx-auto border p-2 mt-10">
-      <label htmlFor="domRef">Dom Ref</label> <br />
+      <label htmlFor="domRef">Dom Ref Example</label> <br />
       <input
         ref={domRef}
         id="domRef"
         type="text"
+        defaultValue={"munna"}
         className="p-3 rounded-full border border-sky-600"
       />
     </div>
